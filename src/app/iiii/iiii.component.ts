86135d9d2f8903b840d010de719289de4c0cc640
InfoDz125 <latifa.bouchekif96@gmail.com>
2024-08-13 17:55:39 +0200

    add IIIIII

---
+++ src/app/iiii/iiii.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iiii',
+  standalone: true,
+  imports: [],
+  templateUrl: './iiii.component.html',
+  styleUrl: './iiii.component.css'
+})
+export class IIIIComponent {
+
+}
